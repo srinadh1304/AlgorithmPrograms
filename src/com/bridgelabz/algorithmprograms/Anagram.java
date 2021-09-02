2c30ecb0b5618724836aafd30a1b108cdd7883a2
srinadh1304 <srinadh1304@gmail.com>
2021-09-02 19:21:08 +0530

    ADDED:Anagram Program

--- src/com/bridgelabz/algorithmprograms/Anagram.java
+++ src/com/bridgelabz/algorithmprograms/Anagram.java
@@ -1,5 +1,45 @@
 package com.bridgelabz.algorithmprograms;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Anagram {
+
+	public boolean checkIfAnagrams(String firstWord, String secondWord) {
+
+		firstWord = firstWord.toLowerCase();
+		secondWord = secondWord.toLowerCase();
+		char[] firstWordArray = firstWord.toCharArray();
+		char[] secondWordArray = secondWord.toCharArray();
+		Arrays.sort(firstWordArray);
+		Arrays.sort(secondWordArray);
+		int compareResult = Arrays.compare(firstWordArray, secondWordArray);
+
+		if(compareResult == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		String firstWord, secondWord;
+		Scanner scannerObject = new Scanner(System.in);
+		Anagram anagramObject = new Anagram();
+		System.out.println("Enter the first word : ");
+		firstWord = scannerObject.next();
+		System.out.println("Enter the second word : ");
+		secondWord = scannerObject.next();
+		scannerObject.close();
+		boolean result = anagramObject.checkIfAnagrams(firstWord, secondWord);
+		if(result)
+			System.out.println("The words are anagrams");
+		else
+			System.out.println("The words are not anagrams");
+
+	}
+
+
 
 }
