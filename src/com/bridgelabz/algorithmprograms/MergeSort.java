1f8fd401dfa9b4e08339199c17e04d684750e40a
srinadh1304 <srinadh1304@gmail.com>
2021-09-03 18:37:51 +0530

    REFACTORED: Given variable names as per Naming rules

--- src/com/bridgelabz/algorithmprograms/MergeSort.java
+++ src/com/bridgelabz/algorithmprograms/MergeSort.java
@@ -7,47 +7,39 @@ public class MergeSort<T extends Comparable<T>> {
 
 	public void merge(T wordArray[], int left, int middle, int right) {
 
-		int sizeOfLeft = middle - left + 1;
+		int sizeOfLeft = middle - left + 1;				
 		int sizeOfRight = right - middle;
-
 		T[] leftArray = (T[]) new Comparable[sizeOfLeft];
 		T[] rightArray = (T[]) new Comparable[sizeOfLeft];
-
 		for (int leftArrayIndex = 0; leftArrayIndex < sizeOfLeft; ++leftArrayIndex)
 			leftArray[leftArrayIndex] = wordArray[left + leftArrayIndex];
-
 		for (int rightArrayIndex = 0; rightArrayIndex < sizeOfRight; ++rightArrayIndex)
 			rightArray[rightArrayIndex] = wordArray[middle + 1 + rightArrayIndex];
-
-		int index1 = 0, index2 = 0;
+		int leftArrayIndex = 0, rightArrayIndex = 0;
 		int sortedArrayindex = left;
-
-		while (index1 < sizeOfLeft && index2 < sizeOfRight) {
-			if (leftArray[index1].compareTo(rightArray[index2]) <= 0) {
-				wordArray[sortedArrayindex] = leftArray[index1];
-				index1++;
+		while (leftArrayIndex < sizeOfLeft && rightArrayIndex < sizeOfRight) {
+			if (leftArray[leftArrayIndex].compareTo(rightArray[rightArrayIndex]) <= 0) {
+				wordArray[sortedArrayindex] = leftArray[leftArrayIndex];
+				leftArrayIndex++;
 			} else {
-				wordArray[sortedArrayindex] = rightArray[index2];
-				index2++;
+				wordArray[sortedArrayindex] = rightArray[rightArrayIndex];
+				rightArrayIndex++;
 			}
 			sortedArrayindex++;
 		}
-
-		while (index1 < sizeOfLeft) {
-			wordArray[sortedArrayindex] = leftArray[index1];
-			index1++;
+		while (leftArrayIndex < sizeOfLeft) {
+			wordArray[sortedArrayindex] = leftArray[leftArrayIndex];
+			leftArrayIndex++;
 			sortedArrayindex++;
 		}
-
-		while (index2 < sizeOfRight) {
-			wordArray[sortedArrayindex] = rightArray[index2];
-			index2++;
+		while (rightArrayIndex < sizeOfRight) {
+			wordArray[sortedArrayindex] = rightArray[rightArrayIndex];
+			rightArrayIndex++;
 			sortedArrayindex++;
 		}
 
 
 	}
-
 	public void mergeSort(T wordArray[], int left, int right) {
 		if (left < right) {
 
